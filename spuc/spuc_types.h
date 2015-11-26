b306a9864c2a7e794cdf364a630734e358e196e1
Tony Kirke <tony@pyspuc.com>
2015-11-26 12:45:36 -0800

    More cleanup

--- spuc/spuc_types.h
+++ spuc/spuc_types.h
@@ -1,5 +1,7 @@
 #pragma once
 // Copyright (c) 2015 Tony Kirke. License MIT  (http://www.opensource.org/licenses/mit-license.php)
 #include <spuc/spuc_defines.h>
-#include <spuce/base_type.h>
-#include <spuc/complex.h>
+#include <cmath>
+#include <spuc/signbit.h>
+#include <spuc/magsq.h>
+#include <complex>
